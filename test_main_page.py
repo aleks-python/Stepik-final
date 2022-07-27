aee02fd3079912a2c6a7b5d94abd620e7bbf0c47
aleks-python <aleks.py@inbox.ru>
2022-07-27 17:42:01 +0300

    Second

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,22 @@
-import time
-
-from selenium import webdriver
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+
+import time
+user_language = "ru"
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    print(f"Start =  {browser.current_url}")
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    print(f"Second =  {browser.current_url}")
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
     el1 = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary")
     el2 = browser.find_element(By.NAME,"login_submit")
     print(f"Last =  {browser.current_url}")
     assert el1 == el2
-    time.sleep(10)
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
